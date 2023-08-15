a6073439f55a374575eb2c22993bfecaa2b2daf6
Shankar S <shankarselvaraj24@gmail.com>
2023-08-15 14:57:41 +0530

    Streamlit working app

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,13 @@ import pandas as pd
 import numpy as np
 import folium
 from folium.plugins import TimestampedGeoJson
+from streamlit_folium import st_folium
 
 def cust():
     df = pd.read_csv("data/raw/sales_enriched2.csv")
     df = df[['sale_date', 'latitude', 'longitude']].sort_values('sale_date').dropna()
     df['sale_date'] = pd.to_datetime(df['sale_date'], format='%d-%m-%Y')
-    m = folium.Map(location=[11.063003, 77.083743], zoom_start=12)
+    m = folium.Map(location=[11.063003, 77.083743], zoom_start=11)
     features = []
 
     for index, row in df.iterrows():
@@ -34,12 +35,13 @@ def cust():
                     period='P10D',  # Animation interval (1 day in this case)
                     add_last_point=True,  # Add the last point after animation
                     auto_play=True).add_to(m)
-    m
+    return m
 
 def main():
     st.set_page_config(layout="wide")
     st.title("Delivery Plot 2021 & 2022")
-    cust()
+    map_ = cust()
+    st_folium(map_, width = 1400)
 
 if __name__ == '__main__':
     main()
